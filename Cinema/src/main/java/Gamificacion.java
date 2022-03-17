1b45bd379c7215dce4b3e81aa3ea5a633dab505b
codeFactory12 <alex.edu.meneses@gmail.com>
2022-03-16 22:36:15 -0400

    Alex Paca

---
+++ Cinema/src/main/java/Gamificacion.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author MENESES
+ */
+public class Gamificacion {
+    int puntos;
+    String retos;
+    String premios;
+    
+}
---
+++ Cinema/src/main/java/Rankins.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author MENESES
+ */
+public class Rankins {
+    String moviename;
+    String coments;
+    public boolean estaEnCartelera(String [] peliculas){
+        boolean valido = false;
+        for(int i = 0; i<peliculas.length -1; i++){
+            if(peliculas[i] == moviename){
+                valido = true;
+            }
+        }
+        return valido;
+    }
+    public void escribirComentario(String nombrePelicula){
+        
+    }   
+}
