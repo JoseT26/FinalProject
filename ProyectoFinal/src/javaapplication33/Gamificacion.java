c9d1242496088af347e561669d9253f4f85b5fbd
codeFactory12 <alex.edu.meneses@gmail.com>
2022-03-23 20:25:54 -0400

    mejora

--- ProyectoFinal/src/javaapplication33/Gamificacion.java
+++ ProyectoFinal/src/javaapplication33/Gamificacion.java
@@ -4,6 +4,8 @@
  */
 package javaapplication33;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Personal
@@ -12,4 +14,86 @@ public class Gamificacion {
     int puntos;
     String retos;
     String premios;
+    String [] reto = new String [10];
+    public int random(){
+        int random = (int)(Math.random()*(3 - 0));
+        return random;
+    }
+    public String retos(){
+        String ans="";
+        int random = (int)(Math.random()*8+1);
+        int ind = 0;
+        switch(random){
+            case 1:
+                ans = "Ver una pelicula de terror";
+                reto[ind] = ans;
+                ind ++;
+                break;
+            case 2:
+                ans = "Comentar en cuatro peliculas";
+                reto[ind] = ans;
+                ind ++;
+                break;
+            case 3:
+                ans = "Ver dos peliculas de comedia";
+                reto[ind] = ans;
+                ind ++;
+                break;
+            case 4:
+                ans = "ver una pelicula de accion";
+                reto[ind] = ans;
+                ind ++;
+                break;
+            case 5:
+                ans = "ver dos peliculas de accion";
+                reto[ind] = ans;
+                ind ++;
+                break;
+            case 6:
+                ans = "ver una pelicula en ingles";
+                reto[ind] = ans;
+                ind ++;
+                break;
+            case 7:
+                ans = "ver una pelicula subtitulada en ingles";
+                reto[ind] = ans;
+                ind ++;
+                break;
+            case 8:
+                ans = "ver una pelicula de drama";
+                reto[ind] = ans;
+                ind ++;
+                break;
+            default:
+                ans = "ver tres pelicutlas";
+                reto[ind] = ans;
+                ind ++;
+        }
+        return ans;
+    }
+    public void menuGamifiacion(){
+        System.out.println("Elija una opcion: ");
+        System.out.println("1. Tomar un reto");
+        System.out.println("2. Ver mis retos");
+        System.out.println("3. Salir");
+        
+        Scanner input = new Scanner(System.in);
+        String option = input.nextLine();
+        if(option == "1"){
+            retos();
+            menuGamifiacion();
+        }else{
+            if(option == "2"){
+                imprimir();
+            }
+        }
+        
+    }
+    public void imprimir(){
+        for(int i = 0; i < reto.length;i++){
+            if(reto[i] != null){
+                System.out.println(reto[i]);
+            }
+        }
+    }
 }
--- ProyectoFinal/src/javaapplication33/Rankins.java
+++ ProyectoFinal/src/javaapplication33/Rankins.java
@@ -3,15 +3,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package javaapplication33;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
  * @author Personal
  */
 public class Rankins {
+    ArrayList <String> coments = new ArrayList <String>();
+    ArrayList <Integer> rank = new ArrayList <Integer>();
     String moviename;
-    String coments;
-    public boolean estaEnCartelera(String [] peliculas){
+    String coment;
+    
+    public boolean peliculaValida(String [] peliculas){
         boolean valido = false;
         for(int i = 0; i<peliculas.length -1; i++){
             if(peliculas[i] == moviename){
@@ -20,7 +25,44 @@ public class Rankins {
         }
         return valido;
     }
-    public void escribirComentario(String nombrePelicula){
-        
-    }   
+    public String escribirComentario(String comentario, String [] peliculas){
+        String ans;
+        if(peliculaValida(peliculas) == true){
+            coments.add(comentario);
+            ans = "Su comentario fue redactado correctamente";
+        }else{
+            ans = "La pelicula no existe";
+        }
+        return ans;
+    }
+    public String addRank(int ranks, String [] peliculas){
+           String ans;
+        if(peliculaValida(peliculas) == true){
+            rank.add(ranks);
+            ans = "Su puntuacion fue redactado correctamente";
+        }else{
+            ans = "La pelicula no existe";
+        }
+        return ans;
+    }
+    public void imprimirComentarios(){
+        for(int i = 0; i<coments.size(); i++){
+            System.out.println(coments.get(i));
+        }
+    }
+     public void imprimirRankin(){
+        for(int i = 0; i<rank.size(); i++){
+            System.out.println(rank.get(i));
+        }
+    }
+   public int rankinDePelicula(){
+       int sum = 0;
+       int num;
+       for(int i=0; i<rank.size();i++){
+           num = rank.get(i);
+           sum = sum + num;
+       }
+       sum = sum / rank.size();
+       return sum;
+   }
 }
