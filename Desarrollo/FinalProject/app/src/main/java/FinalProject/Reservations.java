0720919c188844181f43240c9080caa6a2c31f1e
JoseT26 <JoseT26>
2022-03-27 03:19:18 -0400

    [FinalProject] Add 3 methods in Class Reservations

--- Desarrollo/FinalProject/app/src/main/java/FinalProject/Reservations.java
+++ Desarrollo/FinalProject/app/src/main/java/FinalProject/Reservations.java
@@ -15,9 +15,9 @@ public class Reservations {
     String user;
     String password;
     String movie;
-    int space; ///posible cambio a char u otro dependiendo como se creen los asientos
+    int space; // posible cambio a char u otro dependiendo como se creen los asientos
     int tickets;
-    String hour; //ejemplo: "16:50" para tomar en cuenta el :
+    String hour; // ejemplo: "16:50" para tomar en cuenta el :
     int ticketNumber;
     
     Asientos asiento = new Asientos();
@@ -28,11 +28,32 @@ public class Reservations {
         this.password = password;
     }
     
+    public String getUser() {
+        System.out.println("Write your full name");
+        Scanner input = new Scanner(System.in);
+        user = input.nextLine();
+        return user;
+    }
+
+    public String getUserName() {
+        System.out.println("Write your user name");
+        Scanner input = new Scanner(System.in);
+        username = input.nextLine();
+        return username;
+    }
+
+    public String getPassword() {
+        System.out.println("Write your password");
+        Scanner input = new Scanner(System.in);
+        password = input.nextLine();
+        return password;
+    } 
+    
     public char[] Ticket() {
         Scanner input = new Scanner(System.in);
-        System.out.println("Escriba su asiento para reservar entre A1-A7 y H-1-7");
+        System.out.println("Escriba su asiento para reservar entre A1-A7 y H1-H7");
         String ticket = input.nextLine();
-        ticket=ticket.toUpperCase();
+        ticket = ticket.toUpperCase();
         //"se ingresa de A-H y un numero 1-7
         char[] ch = new char[2];
         for (int i = 0; i < 2; i++) {
@@ -46,18 +67,18 @@ public class Reservations {
         Client cliente = new Client(username, user, password);
         char[]abc = new char[26];
         
-        for ( int i = 0; i < 26; i++) {
+        for (int i = 0; i < 26; i++) {
             abc[i] = (char) ('A' + i );
         }
         int aux = 0;
         
-        for(int i = 0;i < 26;i++) {
-            if(abc[i] == pos[0]) {
+        for (int i = 0; i < 26; i++) {
+            if (abc[i] == pos[0]) {
                 aux=  i;
                 break;
             }
         }
-        asiento.asignarAsiento(aux,Character.getNumericValue(pos[1]),cliente.getName());
+        asiento.asignarAsiento(aux, Character.getNumericValue(pos[1]), cliente.getName());
         return Seat;
     }
 }
